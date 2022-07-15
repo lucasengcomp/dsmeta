0ebedea9ea339202a65a867f066bb03fbb52ca25
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-15 11:48:04 -0300

    Database

--- backend/src/main/java/com/devsuperior/dsmeta/core/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmeta/core/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsmeta.config;
+package com.devsuperior.dsmeta.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
